87eb724278b9999e0bb70d001a59af96f042f49a
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-05-21 02:03:46 +0530

    FEAT: Added filters for get all flights

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -9,41 +9,41 @@ class FlightRepository extends CrudRepository {
         super(Flight);
     }
 
-    async getAllFlights(filter, sort) {
+    async getAllFlights(filter,sort) {
         const response = await Flight.findAll({
             where: filter,
             order: sort,
-            include: [
-                {
-                    model: Airplane,
-                    required: true,
-                    as: 'airplaneDetail',
-                },
-                {
-                    model: Airport,
-                    required: true,
-                    as: 'departureAirport',
-                    on : {
-                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
-                    },
-                    include: {
-                        model: City,
-                        required: true
-                    }
-                },
-                {
-                    model: Airport,
-                    required: true,
-                    as: 'arrivalAirport',
-                    on : {
-                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
-                    },
-                    include: {
-                        model: City,
-                        required: true
-                    }
-                }
-            ]
+            // include: [
+            //     {
+            //         model: Airplane,
+            //         required: true,
+            //         as: 'airplaneDetail',
+            //     },
+            //     {
+            //         model: Airport,
+            //         required: true,
+            //         as: 'departureAirport',
+            //         on : {
+            //             col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+            //         },
+            //         include: {
+            //             model: City,
+            //             required: true
+            //         }
+            //     },
+            //     {
+            //         model: Airport,
+            //         required: true,
+            //         as: 'arrivalAirport',
+            //         on : {
+            //             col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+            //         },
+            //         include: {
+            //             model: City,
+            //             required: true
+            //         }
+            //     }
+            // ]
         });
         return response;
     }
