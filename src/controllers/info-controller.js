2bb928ae9355d6c7164089b30e901fc99ba0d3a8
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2023-06-03 23:03:52 +0530

    made proxy complaint

--- src/controllers/info-controller.js
+++ src/controllers/info-controller.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 
 const info = (req,res) => {
+    console.log('req received');
     return res.status(StatusCodes.OK).json({
         success: true, //checks 
         message: 'Api is Live', //custom message
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {City}  =require('./models')
+const { City } = require('./models')
 const { ServerConfig, Logger } = require('./config')
 const apiRoutes = require('./routes')
 const app = express();
@@ -9,12 +9,17 @@ const app = express();
 
 app.use(express.json()); // helps to parse incoming json body
 app.use(express.urlencoded({ extended: true }))
-app.use('/api', apiRoutes);
 
+app.use('/api', apiRoutes);
+// app.use('/flightsService/api', apiRoutes);
+
+// app.get('/home', (req, res) => {
+//     return res.json({ msg: 'ok' });
+// })
 
 // console.log(process.env);
 app.listen(ServerConfig.PORT, () => {
     console.log(`Suceesfully starting server on port: ${ServerConfig.PORT}`);
-    
+
 })
 
